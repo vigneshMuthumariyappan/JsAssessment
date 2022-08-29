86b401fba23e0ffdbc07c71192f53d37196dc856
vigneshMuthumariyappan <vignesh.muthumariyappan@ziffity.com>
2022-08-29 18:04:19 +0530

    create carosule.js

---
+++ carosule.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        centerMode:true,
+        centerPadding:'30px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        draggable:true,
+        arrows:true,
+         });
+    });
